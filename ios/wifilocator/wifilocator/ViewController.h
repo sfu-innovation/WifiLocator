0d7ae9aaeaafa6cc2c417c67d4a6086669cd4921
Catherine Tan <llt3@sfu.ca>
2012-05-17 14:15:28 -0700

    Attempting to fix iOS sync

---
+++ ios/wifilocator/wifilocator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  wifilocator
+//
+//  Created by  on 12-05-17.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
