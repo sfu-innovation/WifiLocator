7f9da0233bfdc1d5da0b69dc9abc0962c727f82e
unknown <mike@mikeklem.(none)>
2012-05-17 12:00:24 -0700

    Updating data with surrey info

--- src/com/sfumobile/wifilocator/WifiLocatorActivity.java
+++ src/com/sfumobile/wifilocator/WifiLocatorActivity.java
@@ -34,7 +34,15 @@ public class WifiLocatorActivity extends Activity implements OnClickListener{
         db = new DBAdapter(this.getApplicationContext());
         db.createDatabase();
         db.openDataBase();
-             
+        
+        Cursor c = db.getAP();
+        c.moveToFirst();
+        for(int i=0; i<c.getCount(); i++){
+        	Log.d("STUFF", c.getString(c.getColumnIndex("bssid")));
+        	c.moveToNext();
+        }
+        c.close();
+        
         bssidText  = (TextView)this.findViewById(R.id.bssidText);
         macText    = (TextView)this.findViewById(R.id.macText);
         zoneText   = (TextView)this.findViewById(R.id.zoneText);
