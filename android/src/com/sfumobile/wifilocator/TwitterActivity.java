ac5f8a916faa211f37b0b4e8544da7687392c479
Mike <mike.klemarewski@gmail.com>
2012-05-21 22:01:37 -0700

    Finished functionality for updating twitter status.  Few bugs here and there

--- android/src/com/sfumobile/wifilocator/TwitterActivity.java
+++ android/src/com/sfumobile/wifilocator/TwitterActivity.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -13,32 +19,54 @@ import android.widget.ArrayAdapter;
 import android.widget.Toast;
 import twitter4j.Query;
 import twitter4j.QueryResult;
+import twitter4j.Status;
 import twitter4j.Tweet;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 
-public class TwitterActivity extends Activity{
+public class TwitterActivity extends Activity implements OnClickListener{
 
 	private Twitter twitter;
-	private SimpleCursorAdapter adapter;
 	private ListView tweetListView;
+	private EditText tweetText;
+	private Button tweetButton;
+	private Button cancelButton;
 	private String zone;
+	private ArrayList<String> tweet_text;
+	private ArrayAdapter<String> adapter;
+	private AccessToken token;
+	private SharedPreferences prefs;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.tweets);
+		setContentView(R.layout.twitter_view);
+		
+		prefs = getSharedPreferences(TwitterSignInActivity.TOKEN_FILE, 0);
 		
 		tweetListView = (ListView)this.findViewById(R.id.TweetListView);
+		tweetText     = (EditText)this.findViewById(R.id.tweetText);
+		cancelButton  = (Button)this.findViewById(R.id.cancelButton);
+		tweetButton   = (Button)this.findViewById(R.id.tweetButton);
+		
+		tweetText.setOnClickListener(this);
+		cancelButton.setOnClickListener(this);
+		tweetButton.setOnClickListener(this);
+		
 		twitter = new TwitterFactory().getInstance();
+		
+		tweetText.setTextColor(Color.GRAY);
 		
         Bundle extras = getIntent().getExtras();
         if(extras!=null){
         	zone = extras.getString("zone");
         }
+		twitter.setOAuthConsumer(TwitterSignInActivity.CONSUMER_KEY, TwitterSignInActivity.CONSUMER_SECRET);
+        token = new AccessToken(prefs.getString("token", ""), prefs.getString("secret", ""));
+		twitter.setOAuthAccessToken(token);
 	}
 
 	@Override
@@ -47,10 +75,10 @@ public class TwitterActivity extends Activity{
 		super.onStart();
 		
 		try {
-			QueryResult result = twitter.search(new Query(zone.replaceAll(" ", "")));
 			Log.d("ZONE",zone.replaceAll(" ", ""));
+			QueryResult result = twitter.search(new Query(zone.replaceAll(" ", "")));
 			List<Tweet> tweets = result.getTweets();
-			ArrayList<String> tweet_text = new ArrayList<String>();
+			tweet_text = new ArrayList<String>();
 			for(Tweet tweet : tweets){
 				tweet_text.add("@" + tweet.getFromUser() + " - " + tweet.getText());
 			}
@@ -59,25 +87,49 @@ public class TwitterActivity extends Activity{
 				tweet_text.add("No Tweets");
 			}
 			
-			ArrayAdapter a = new ArrayAdapter(this.getApplicationContext(), R.layout.tweet_row, R.id.tweetText, tweet_text);
-			tweetListView.setAdapter((ListAdapter)a);
+			adapter = new ArrayAdapter<String>(this.getApplicationContext(), R.layout.tweet_row, R.id.tweetText, tweet_text);
+			tweetListView.setAdapter((ListAdapter)adapter);
 		} catch (TwitterException e) {
 			// TODO Auto-generated catch block
 			Log.e("Twitter", e.getMessage());
+			
+			tweet_text = new ArrayList<String>();
+			tweet_text.add("No Tweets");
+			adapter = new ArrayAdapter<String>(this.getApplicationContext(), R.layout.tweet_row, R.id.tweetText, tweet_text);
+			tweetListView.setAdapter((ListAdapter)adapter);
 		}
 		
 	}
 	
-	void tweet (AccessToken token, String message){
-		Log.d("TWEET", "Tweeting!");
+	void tweet (String message){
 		if (token != null) {
+			Status status = null;
 			try {
-				twitter.setOAuthAccessToken(token);
-				twitter.updateStatus(message);
+				status = twitter.updateStatus(message);
 			} catch (Exception e) {
 				Toast.makeText(this, "Error:" + e.getMessage(),Toast.LENGTH_LONG).show();
 				Log.d("Timeline",""+e.getMessage());
 			}
+			if(status != null){
+				Toast.makeText(this, "Updated Twitter Status",Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.tweetText:
+			tweetText.setText("");
+			tweetText.setTextColor(Color.BLACK);
+			break;
+		case R.id.cancelButton:
+			tweetText.setText("Tweet");
+			tweetText.setTextColor(Color.GRAY);
+			break;
+	
+		case R.id.tweetButton:
+			tweet("#" + zone + " " + tweetText.getText().toString());
+			break;
 		}
 	}
 
--- android/src/com/sfumobile/wifilocator/TwitterSignInActivity.java
+++ android/src/com/sfumobile/wifilocator/TwitterSignInActivity.java
@@ -10,6 +10,8 @@ import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,16 +22,20 @@ public class TwitterSignInActivity extends Activity {
 
 	Twitter twitter;
 	RequestToken requestToken;
-	public final static String consumerKey = "5wNUxrYGUQ2SiPYrUB1w";
-	public final static String consumerSecret = "49CfLOBUnvIubWwh37FgcWJZR3SqxXkgooHHvnRF4s"; 
+	public final static String TOKEN_FILE = "access_token";
+	public final static String CONSUMER_KEY = "5wNUxrYGUQ2SiPYrUB1w";
+	public final static String CONSUMER_SECRET = "49CfLOBUnvIubWwh37FgcWJZR3SqxXkgooHHvnRF4s"; 
 	private final String CALLBACKURL = "SFUMobile://wifilocator";
 	private String zone;
+	private SharedPreferences prefs;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sign_in);
 		
+        prefs = getSharedPreferences(TOKEN_FILE,0);
+
         Bundle extras = getIntent().getExtras();
         if(extras!=null){
         	zone = extras.getString("zone");
@@ -39,31 +45,38 @@ public class TwitterSignInActivity extends Activity {
 	}
 
 	public void login() {
-		try {
-			twitter = new TwitterFactory().getInstance();
-			twitter.setOAuthConsumer(consumerKey, consumerSecret);
-			requestToken = twitter.getOAuthRequestToken(CALLBACKURL);
-			String authUrl = requestToken.getAuthorizationURL();
-			this.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl)));
-		} catch (TwitterException e) {
-			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-			Log.e("Login", e.getMessage());
+		if(prefs.equals(0)){
+			try {
+				twitter = new TwitterFactory().getInstance();
+				twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+				requestToken = twitter.getOAuthRequestToken(CALLBACKURL);
+				String authUrl = requestToken.getAuthorizationURL();
+				this.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl)));
+			} catch (TwitterException e) {
+				Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+				Log.e("Login", e.getMessage());
+			}
 		}
 	}
 
 	@Override
+	protected void onStart(){
+		super.onStart();
+		Intent myIntent = new Intent(this.getApplicationContext(), TwitterActivity.class);
+		myIntent.putExtra("zone", zone);
+		startActivity(myIntent);
+	}
+	
+	@Override
 	protected void onNewIntent(Intent intent) {
 		super.onNewIntent(intent);
 		Uri uri = intent.getData();
-		Log.d("RESUME", "Resuming!");
 		try {
 			if(uri != null){
 				String verifier = uri.getQueryParameter("oauth_verifier");
 				AccessToken accessToken = twitter.getOAuthAccessToken(requestToken, verifier);
-				displayTimeLine(accessToken);
-				Intent myIntent = new Intent(this.getApplicationContext(), TwitterActivity.class);
-				myIntent.putExtra("zone", zone);
-				startActivity(myIntent);
+				saveToken(accessToken);
+				//displayTimeLine(accessToken);
 			}
 		} catch (TwitterException e) {
 			Log.e("onNewIntent", "" + e.getMessage());
@@ -85,5 +98,12 @@ public class TwitterSignInActivity extends Activity {
 		} else {
 			Toast.makeText(this, "Not Verified", Toast.LENGTH_LONG).show();
 		}
+	}
+	
+	void saveToken(AccessToken token){
+		final Editor edit = prefs.edit();
+		edit.putString("token", token.getToken());
+		edit.putString("secret", token.getTokenSecret());
+		edit.commit();
 	}
 }
