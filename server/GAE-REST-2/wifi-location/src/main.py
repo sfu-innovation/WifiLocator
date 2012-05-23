68ca80d78947b4ed75a917aa7340085e07ffd18b
jrf2 <jrf2@sfu.ca>
2012-05-23 12:28:46 -0700

    adding getzone and getfriends features to repo

--- server/GAE-REST-2/wifi-location/src/main.py
+++ server/GAE-REST-2/wifi-location/src/main.py
@@ -5,6 +5,7 @@ import urllib
 import wsgiref.handlers
 import csv
 import rest
+from django.utils import simplejson
 
 from src.models import Zones, ZoneNames, Friends, Areas, BSSIDZones
 from google.appengine.ext.webapp import template
@@ -50,9 +51,37 @@ class MainPage(webapp.RequestHandler):
 		path = os.path.join(os.path.dirname(__file__), 'index.html')
 		self.response.out.write(template.render(path,template_values))
 
+class BSSIDHandler(webapp.RequestHandler):
+	def get(self, mac_address):
+		allbssid = BSSIDZones.all()
+		filtered = allbssid.filter("mac_address = ", urllib.unquote_plus(mac_address))
+		data = simplejson.dumps({'error' : "no data"})
+		for item in filtered:
+			curr_zone = item.zones
+			zone_id = curr_zone.zone_id
+			data = simplejson.dumps({'zone' : zone_id, 'zone_name' : curr_zone.zone_name, 'mac_address' : item.mac_address})
+			break;
+		self.response.headers['Content-Type'] = "application/json"
+		self.response.out.write(data)
 
+class FriendHandler(webapp.RequestHandler):
+	def get(self, username):
+		allfriends = Friends.all()
+		filtered = allfriends.filter("user_name = ", username)
+		data = "{ 'friends' : ["
+		iter = 0
+		for item in filtered:
+			data += simplejson.dumps({'friend_name' : item.friend_name})
+			iter += 1
+			if iter != filtered.count():
+				data += ", "
+		data += "]}"
+		self.response.headers['Content-Type'] = "application/json"
+		self.response.out.write(data)
+		
+application = webapp.WSGIApplication([('/', MainPage), ('/rest/.*', rest.Dispatcher), ('/getzone/(.*)', BSSIDHandler), ('/getfriends/(.*)', FriendHandler)], debug=True)
 
-application = webapp.WSGIApplication([('/', MainPage), ('/rest/.*', rest.Dispatcher)], debug=True)
+#application = webapp.WSGIApplication([('/', MainPage), ('/rest/.*', rest.Dispatcher)], debug=True)
 
 # configure the rest dispatcher to know what prefix to expect on request urls
 rest.Dispatcher.base_url = "/rest"
