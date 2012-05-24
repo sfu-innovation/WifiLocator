99d13bb9583ac1cf18627f8b0566786940a2c4be
Catherine Tan <llt3@sfu.ca>
2012-05-24 15:35:07 -0700

    commit again

--- server/GAE-REST-2/wifi-location/src/friends.py
+++ server/GAE-REST-2/wifi-location/src/friends.py
@@ -11,6 +11,7 @@ from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+
 class FriendHandler(webapp.RequestHandler):
 	def get(self, user_short_name):
 		data = dict()
