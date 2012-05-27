cd708fb8ba6fee279e0add498370e04112d4a946
jrf2 <jrf2@sfu.ca>
2012-05-27 15:47:13 -0700

    updating setfriend method to return json responses and create/delete bidirectional friend relationships

--- server/GAE-REST-2/wifi-location/src/friends.py
+++ server/GAE-REST-2/wifi-location/src/friends.py
@@ -45,28 +45,53 @@ class FriendHandler(webapp.RequestHandler):
 
 class SetFriend(webapp.RequestHandler):
 	def get(self):
-		self.response.out.write("requires post or delete request")
+		data = {'status' : 1, 'message' : 'requires post request'}
+		self.response.out.write(json.dumps(data))
 	def post(self):
 		json_obj = json.loads(self.request.body)
+		self.response.headers['Content-Type'] = "application/json"
+		data = {}
 		
-		userobj = db.GqlQuery(("SELECT * FROM Users " + "WHERE short_name = :1"), urllib.unquote_plus(json_obj["user_name"]))
+		# get user
+		userobj = db.GqlQuery(("SELECT * FROM Users WHERE short_name = :1"), urllib.unquote_plus(json_obj["user_name"]))
 		if userobj.count() <= 0:
-			self.response.out.write("user_not_found")
+			data = {'status' : 1, 'message' : 'user not found'}
+			self.response.out.write(json.dumps(data))
 			return
-		friendobj = db.GqlQuery(("SELECT * FROM Users " + "WHERE short_name = :1"), urllib.unquote_plus(json_obj["friend"]))
+		
+		# get friend
+		friendobj = db.GqlQuery(("SELECT * FROM Users WHERE short_name = :1"), urllib.unquote_plus(json_obj["friend"]))
 		if userobj.count() <= 0:
-			self.response.out.write("friend_not_found")
+			data = {'status' : 1, 'message' : 'friend not found'}
+			self.response.out.write(json.dumps(data))
 			return
 			
+		# check if friendship relationship already exists
+		friendentry = db.GqlQuery(("SELECT * FROM Friends WHERE user = :1 and friend_id = :2"), 
+			userobj[0].key(), friendobj[0].key().id())
+		friendentry2 = db.GqlQuery(("SELECT * FROM Friends WHERE user = :1 and friend_id = :2"), 
+			friendobj[0].key(), userobj[0].key().id())
+		if friendentry.count() > 0 and json_obj["command"] == "addfriend":
+			data = {'status' : 1, 'message' : 'entry already exists'}
+			self.response.out.write(json.dumps(data))
+			return
+		elif (friendentry.count() <= 0 or friendentry2.count() <= 0) and json_obj["command"] == "deletefriend":
+			data = {'status' : 1, 'message' : 'no entry found to delete'}
+			self.response.out.write(json.dumps(data))
+			return
+			
+		# preform operation
 		if json_obj["command"] == "addfriend":
 			Friends(user = userobj[0].key(), friend_id = friendobj[0].key().id()).put()
-			self.response.out.write("add_Success")
+			Friends(user = friendobj[0].key(), friend_id = userobj[0].key().id()).put()
+			data = {'status' : 0, 'message' : 'add friend successful'}
 		elif json_obj["command"] == "deletefriend":
-			friendentry = db.GqlQuery(("SELECT * FROM Friends " + "WHERE user = :1 AND friend_id = :2"), userobj[0].key(), friendobj[0].key().id())
-			if friendentry.count() <= 0:
-				self.response.out.write("entry_not_found")
 			friendentry[0].delete()
-			self.response.out.write("delete_success")
+			friendentry2[0].delete()
+			data = {'status' : 0, 'message' : 'delete friend successful'}
 		else:
-			self.response.out.write("invalid_command")
-
+			data = {'status' : 1, 'message' : 'command invalid'}
+			
+		self.response.out.write(json.dumps(data))
+		
+		
