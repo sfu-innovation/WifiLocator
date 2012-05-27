55c6f579735db06b3a637fbfcbd8491d42cc3c05
hrl1 <hrl1@sfu.ca>
2012-05-27 16:05:45 -0700

    SSIDs list

---
+++ android/src/com/sfumobile/wifilocator/SSIDs.java
@@ -0,0 +1,7 @@
+package com.sfumobile.wifilocator;
+
+import java.util.*;
+
+public class SSIDs {
+	public static final Set<String> SSIDs = new HashSet<String>(Arrays.asList(new String[] {"SFUNET", "SFUNET-SECURE", "eduroam"}));
+}
