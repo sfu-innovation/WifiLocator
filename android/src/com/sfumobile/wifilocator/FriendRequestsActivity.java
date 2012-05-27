e31d4fb332a51ca8da1051e36866d04f3ebb441f
Mike <mike.klemarewski@gmail.com>
2012-05-27 13:41:39 -0700

    Creating view friend requests activity

---
+++ android/src/com/sfumobile/wifilocator/FriendRequestsActivity.java
@@ -0,0 +1,21 @@
+package com.sfumobile.wifilocator;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class FriendRequestsActivity extends ListActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.friend_requests);
+		
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+
+	
+}
