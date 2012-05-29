b2ab3ae6747c469a3d5ec15fd2c2f8d98652b878
jrf2 <jrf2@sfu.ca>
2012-05-28 23:14:00 -0700

    updating setusers command to reveal application/json response type and check for existence of entry before creating/deleting table entry

--- server/GAE-REST-2/wifi-location/src/users.py
+++ server/GAE-REST-2/wifi-location/src/users.py
@@ -20,19 +20,27 @@ class SetUser(webapp.RequestHandler):
 		
 	def post(self):
 		json_obj = json.loads(self.request.body)
+		self.response.headers['Content-Type'] = "application/json"
 		data = {}
 		
+		# check if user exists
+		userobj = db.GqlQuery(("SELECT * FROM Users " + "WHERE short_name = :1"), urllib.unquote_plus(json_obj["short_name"]))
+		if json_obj["command"] == "adduser" and userobj.count() > 0:
+			data = {'status' : 1, 'message' : 'user already exists'}
+			self.response.out.write(json.dumps(data))
+			return
+		elif json_obj["command"] == "deleteuser" and userobj.count() <= 0:
+			data = {'status' : 1, 'message' : 'user not found'}
+			self.response.out.write(json.dumps(data))
+			return
+		
+		# preform operation
 		if json_obj["command"] == "adduser":
 			Users(first_name = json_obj["first_name"], short_name = json_obj["short_name"], last_name = json_obj["last_name"]).put()
 			data = {'status' : 0, 'message' : 'user added successfully'}
 		elif json_obj["command"] == "deleteuser":
-			userobj = db.GqlQuery(("SELECT * FROM Users " + "WHERE short_name = :1"), urllib.unquote_plus(json_obj["short_name"]))
-			if userobj.count() <= 0:
-				data = {'status' : 1, 'message' : 'user not found'}
-				self.response.out.write("user_not_found")
-			else:
-				userobj[0].delete()
-				data = {'status' : 0, 'message' : 'user deleted successfully'}
+			userobj[0].delete()
+			data = {'status' : 0, 'message' : 'user deleted successfully'}
 		else:
 			data = {'status' : 1, 'message' : 'invalid command'}
 		self.response.out.write(json.dumps(data))
