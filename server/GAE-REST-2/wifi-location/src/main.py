f9e466f2d85910467de85435dedca18e174717ef
jrf2 <jrf2@sfu.ca>
2012-05-30 10:43:19 -0700

    added mapname to getfriendslist response and started writing a testcase for testing friendship creation

--- server/GAE-REST-2/wifi-location/src/main.py
+++ server/GAE-REST-2/wifi-location/src/main.py
@@ -238,7 +238,8 @@ application = webapp.WSGIApplication([
 									('/request/(.*)', RequestHandler),
 									('/accept/(.*)', AcceptHandler),
 									('/getmap/(.*)', MapHandler),
-									('/rest/.*', rest.Dispatcher), 
+									('/rest/.*', rest.Dispatcher),
+									('/getuserid/', GetUserId),
 									('/setfriend/', SetFriend),
 									('/setuser/', SetUser)],
 									debug=True)
