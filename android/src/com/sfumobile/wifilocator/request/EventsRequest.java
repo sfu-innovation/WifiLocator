2d2aebdd41a695fb29639edb4755e28462b7834c
Mike <mike.klemarewski@gmail.com>
2012-05-31 10:02:50 -0700

    Implementing events

---
+++ android/src/com/sfumobile/wifilocator/request/EventsRequest.java
@@ -0,0 +1,25 @@
+package com.sfumobile.wifilocator.request;
+
+import com.sfumobile.wifilocator.types.RequestTypes;
+
+import android.os.Handler;
+
+public class EventsRequest extends Request{
+
+	private String _id;
+	private String _mac_address;
+	
+	public EventsRequest(String id, String mac_address) {
+		super(RequestTypes.EVENTS_REQUEST);
+		_id = id;
+		_mac_address = mac_address;
+	}
+
+	@Override
+	public String getURL() {
+		setProperty( "user_id", ""+_id, RequestTypes.INT_TYPE);
+		setProperty( "mac_address", ""+_mac_address, RequestTypes.STRING_TYPE);
+		return RequestConstants.GET_EVENTS_URL;
+	}
+
+}
